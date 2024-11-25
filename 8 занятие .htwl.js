53cd435674fcdcea35936fe77a6aacbf0a50de34
ilogic-10-p <fdor_filippov_2025@vk.com>
2024-11-25 16:01:41 +0300

    я изучас скс

---
+++ 8 занятие .htwl.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html> ь
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div id="div">Текст для примера</div>
+<input id="input" type="text">
+    <!--<input type="number">-->
+    <!--<input type="password">-->
+    <!--<input type="tel">-->
+    <script src="8%20занятие.js"></script>
+</body>
+</html>
